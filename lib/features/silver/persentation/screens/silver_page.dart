5e414c63f2a0f0e4fae0a4cd63a3f7745eddc7f4
sherif-bahaa <sherifbahaa939@gmail.com>
2026-02-12 17:46:37 +0200

    Refactor SilverPage to use Bloc for state management and enhance UI structure

--- lib/features/silver/persentation/screens/silver_page.dart
+++ lib/features/silver/persentation/screens/silver_page.dart
@@ -1,28 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:goldy/core/costants/app_color.dart';
-import 'package:goldy/features/gold/persentation/screens/widgets/custom_text.dart';
+import 'package:goldy/features/silver/persentation/cubit/silver_cubit.dart';
 
 class SilverPage extends StatelessWidget {
   const SilverPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Silver tracker',
-          style: TextStyle(color: AppColors.sliverColor),
-        ),
-      ),
-      body: Column(
-        children: [
-          Image.network(
-            'https://media.istockphoto.com/id/468068368/photo/stack-of-shiny-silver-bars-inside-a-bank-vault.jpg?s=2048x2048&w=is&k=20&c=Iszr5PG7JI-VCTLx_z-eV52mcTgcUWHMgq2S2RAjV4o=',
-            width: 100,
-            height: 100,
+    return BlocProvider(
+      create: (context) => SilverCubit()..fetchSilverPrice(),
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Center(
+            child: const Text(
+              'Silver tracker',
+              style: TextStyle(color: AppColors.sliverColor),
+            ),
           ),
-          CustomText(text: '100 USD'),
-        ],
+        ),
+        body: BlocBuilder<SilverCubit, SilverState>(
+          builder: (context, state) {
+            if (state is SilverError) {
+              return Center(child: Text(state.message));
+            }
+            if (state is SilverSuccess) {
+              return Column(
+                children: [
+                  Image.network(
+                    'https://media.istockphoto.com/id/468068368/photo/stack-of-shiny-silver-bars-inside-a-bank-vault.jpg?s=2048x2048&w=is&k=20&c=Iszr5PG7JI-VCTLx_z-eV52mcTgcUWHMgq2S2RAjV4o=',
+                    width: 100,
+                    height: 100,
+                  ),
+                  Text(
+                    '100 USD',
+                    style: TextStyle(
+                      color: AppColors.sliverColor,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              );
+            }
+            return Center(child: Text('Loading...'));
+          },
+        ),
       ),
     );
   }
